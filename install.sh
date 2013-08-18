f71dc7c405fac4163406f25431b927ef95153333
Birunthan Mohanathas <birunthan@mohanathas.com>
2013-08-18 11:43:51 +0300

    Add the dot to the dotfiles

--- install.sh
+++ install.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 declare -A map
-map["src/bashrc"]=".bashrc"
-map["src/gitconfig"]=".gitconfig"
-map["src/hgrc"]=".hgrc"
-map["src/vim"]=".vim"
-map["src/vimrc"]=".vimrc"
-map["src/xfce-terminalrc"]=".config/Terminal/terminalrc"
+map["src/.bashrc"]=".bashrc"
+map["src/.gitconfig"]=".gitconfig"
+map["src/.hgrc"]=".hgrc"
+map["src/.vim"]=".vim"
+map["src/.vimrc"]=".vimrc"
+map["src/.xfce-terminalrc"]=".config/Terminal/terminalrc"
 
 for file in "${!map[@]}"
 do
