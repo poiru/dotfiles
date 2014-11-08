f116370a4ba8cdfa808ea578fe814917cc44275d
Birunthan Mohanathas <birunthan@mohanathas.com>
2014-11-08 11:37:02 +0200

    osx: Add plist.sh and import some plists

---
+++ osx/plist.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+plists=(
+    "com.divisiblebyzero.Spectacle.plist"
+    "com.ragingmenace.MenuMeters.plist"
+    "org.pqrs.Karabiner.plist"
+    "org.pqrs.Seil.plist"
+)
+
+for plist in "${plists[@]}"; do
+    local_file="$script_dir/plist/$plist"
+    library_file="$HOME/Library/Preferences/$plist"
+    if [[ "$1" == "export" ]]; then
+       plutil -convert xml1 "$library_file" -o "$local_file"
+    fi
+done
