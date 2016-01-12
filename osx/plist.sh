02a75cd4a2255530033d4e9a610a05d5dedbd483
Birunthan Mohanathas <birunthan@mohanathas.com>
2016-01-12 10:54:14 -0800

    osx: Make plish.sh warn about unrecognized options

--- osx/plist.sh
+++ osx/plist.sh
@@ -10,15 +10,18 @@ plists=(
     "com.googlecode.iterm2"
     "org.macosforge.xquartz.X11"
     "org.pqrs.Karabiner"
-    "org.pqrs.Seil.plist"
+    "org.pqrs.Seil"
 )
 
 for plist in "${plists[@]}"; do
     local_file="$script_dir/plist/$plist.plist"
     if [[ "$1" == "save" ]]; then
-       library_file="$HOME/Library/Preferences/$plist.plist"
-       plutil -convert xml1 "$library_file" -o "$local_file"
+        library_file="$HOME/Library/Preferences/$plist.plist"
+        plutil -convert xml1 "$library_file" -o "$local_file"
     elif [[ "$1" == "apply" ]]; then
         defaults import "$plist" "$script_dir/plist/$plist.plist"
+    else
+        echo "Invalid option: $1 (use save/apply)"
+        exit
     fi
 done
