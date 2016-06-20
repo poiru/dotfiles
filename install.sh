4ab4325442b30b16fc13ae6240995e07a87fde7a
Birunthan Mohanathas <birunthan@mohanathas.com>
2016-06-20 11:13:47 -0700

    xcode: Add editor theme

--- install.sh
+++ install.sh
@@ -87,6 +87,7 @@ function link_local() {
     elif [[ "$OSTYPE" == "darwin"* ]]; then
         make_link "karabiner/private.xml" "Library/Application Support/Karabiner/private.xml"
         make_link "sublime-text/User" "Library/Application Support/Sublime Text 3/Packages/User"
+        make_link "xcode/Tomorrow Night.dvtcolortheme" "Library/Developer/Xcode/UserData/FontAndColorThemes/Tomorrow Night.dvtcolortheme"
         make_link "xquartz/.gtkrc-2.0"
         make_link "xquartz/.Xmodmap"
         make_link "xquartz/.Xresources"
